82ab95f615c5c696fc651cbbf1af21025a5259f2
Alexstericris <42732998+Alexstericris@users.noreply.github.com>
2018-11-05 10:25:47 +0100

    Create mainv.java

---
+++ mainv.java
@@ -0,0 +1,11 @@
+class mainv{
+  public static void main(String[]args){
+  essen Gurke= new gurke();
+  essen grunzeug[]= new essen[2];
+  grunzeug[0]=new gurke();
+  grunzeug[1]=new apfel();
+  
+  for(int x=0;x<2;x++){
+  grunzeug[x].eat();
+  }
+}
